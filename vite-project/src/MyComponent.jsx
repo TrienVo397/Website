de34c947bf169ff4bb16deeed7b0dc808257a10a
TrienVo397 <s3907397@rmit.edu.vn>
2024-09-26 14:24:50 +0700

    Up

--- vite-project/src/MyComponent.jsx
+++ vite-project/src/MyComponent.jsx
@@ -1,38 +1,29 @@
-import React,{useState} from 'react';
+import React, { useState } from "react";
 
-function MyComponent(){
-    const[car, setCar] = useState ({
-        year: 2024,
-        make:"Ford",
-        model:"Mustang"
-    })
+function MyComponent() {
+  const [foods, setFoods] = useState(["Apple", "Orange", "Banana"]);
+  function handleAddFood(event) {
+    const newFood = document.querySelector("#foodInput").value;
+    document.querySelector("#foodInput").value = ""; // to make the text field empty
+    setFoods(f => [...f, newFood]); // updater function with spread operator
+  }
+  function handleRemoveFood(index) {
+    setFoods(foods.filter((_,i) => i!==index)) 
+    //Using underscore is a convention that indicates the param being passed to the function is ignored
+  }
 
-    function handleYearChange(event){
-        // setCar({year: 2024, make:"Ford" , model: "Mustang", year:2025})
-        //... is Spread Operator
-        setCar(c=>({...c, year: event.target.value})) // exactly like above but shorter
-        
-    }
-
-    function handleMakeChange(event){
-    setCar(c =>({...c, make:event.target.value}))
-
-    }
-
-    function handleModelChange(event){
-        setCar(c =>({...c, model:event.target.value}))
-    }
-
-    return(
-        <div>
-            <p>Your favorite car is {car.year} {car.make} {car.model}</p>
-            <input type="number" value={car.year} onChange={handleYearChange} />
-            <input type="text" value={car.make} onChange={handleMakeChange}  />
-            <input type="text" value={car.model} onChange={handleModelChange} />
-        </div>
-    )
-
-
+  return (
+    <div>
+      <h2>List of Food</h2>
+      <ul>
+        {foods.map((food, index) => (
+          <li key={index} onClick = {()=>handleRemoveFood(index)}>{food} </li>
+        ))}
+      </ul>
+      <input type="text" id="foodInput" placeholder="Enter food name" />
+      <button onClick={handleAddFood}>Add Food</button>
+    </div>
+  );
 }
 
-export default MyComponent
+export default MyComponent;
